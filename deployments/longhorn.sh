496e6de8604a7e5ee8c055a37acd65b9cabb3ab7
JamesClonk <jamesclonk@jamesclonk.ch>
2023-07-27 14:01:43 +0200

    add oauth2 to longhorn

--- deployments/longhorn.sh
+++ deployments/longhorn.sh
@@ -35,12 +35,14 @@ longhorn_volume_replicas=$(max -g 1 $(min -g ${initial_machinedeployment_replica
 # is VCD or Longhorn going to be the default storage class?
 longhorn_default_class=$(kubectl get storageclass -A -o jsonpath='{.items[?(@.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")].metadata.name}' | grep 'vcd' >/dev/null && echo "false" || echo "true")
 
+cluster_hostname=$(yq -e eval '.kubernetes.hostname' config.yaml)
 cat > "deployments/${chart}.values.yaml" <<EOF
 persistence:
   defaultClass: ${longhorn_default_class}
   defaultFsType: ext4
   defaultClassReplicaCount: ${longhorn_volume_replicas}
   defaultReplicaAutoBalance: least-effort
+
 defaultSettings:
   kubernetesClusterAutoscalerEnabled: true
   defaultReplicaCount: ${longhorn_volume_replicas}
@@ -53,11 +55,25 @@ defaultSettings:
   snapshotDataIntegrityCronjob: 0 3 * * *
   snapshotDataIntegrityImmediateCheckAfterSnapshotCreation: false
   upgradeChecker: false
+
+ingress:
+  enabled: true
+  ingressClassName: nginx
+  host: longhorn.${cluster_hostname}
+  tls: true
+  secureBackends: false
+  tlsSecret: longhorn-tls
+  path: /
+  annotations:
+    nginx.ingress.kubernetes.io/ssl-redirect: "true"
+    nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
+    nginx.ingress.kubernetes.io/auth-signin: "https://oauth2-proxy.${cluster_hostname}/oauth2/start"
+    nginx.ingress.kubernetes.io/auth-url: "https://oauth2-proxy.${cluster_hostname}/oauth2/auth"
+    cert-manager.io/cluster-issuer: "lets-encrypt"
 EOF
 deployments/install-chart.sh "${repository}" "${chart}" "${namespace}" "${version}" "deployments/${chart}.values.yaml"
 
 echo " "
 echo "================================================================================================================="
-echo "Longhorn has been installed ..."
-echo "To access, open a port-forwarding by running: kubectl -n longhorn-system port-forward svc/longhorn-frontend 9999:80"
+echo "Longhorn has been installed, visit: https://longhorn.${cluster_hostname}"
 echo "================================================================================================================="
