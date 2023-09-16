75c9492b1e9d7c91d05ff15aaf19e16a16dad0df
JamesClonk <jamesclonk@jamesclonk.ch>
2023-09-16 17:07:19 +0200

    add wireguard configuration

--- deployments/wireguard.sh
+++ deployments/wireguard.sh
@@ -12,16 +12,15 @@ chart="wireguard"
 version="0.0.1"
 namespace="${chart}"
 
+wireguard_server_address=$(yq -e eval '.kubernetes.wireguard.serverAddress // "10.242.42.1/24"' config.yaml)
+wireguard_server_key=$(yq -e eval '.kubernetes.wireguard.privateKey' config.yaml)
 cat > "deployments/${chart}.values.yaml" <<EOF
 wireguard:
-  serverAddress: 10.242.42.1/24
-  privateKey: aFNRgUHsMqyrj7cwWwsSKQvkEgXqTbJxiuTOjU3KB1c=
-
-  clients:
-  - name: workstation
-    publicKey: pTAAvK3WkMy1MHgTlWJCdvoNpMSEy/WnfNblV96XUQw=
-    allowedIPs: 10.242.42.5/32
+  serverAddress: ${wireguard_server_address}
+  privateKey: ${wireguard_server_key}
+  clients: []
 EOF
+yq -e eval '.wireguard.clients = (load("config.yaml") | .kubernetes.wireguard.clients // [])' -i "deployments/${chart}.values.yaml"
 deployments/install-chart.sh "${repository}" "${chart}" "${namespace}" "${version}" "deployments/${chart}.values.yaml"
 
 echo " "
